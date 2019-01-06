92753f4d6fd19733c241904c906980cf9394b65d
Libor Kramolis <libor.kramolis@gmail.com>
2019-01-06 14:39:31 +0100

    Using Gradle to build the project.

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:26.0-jre")
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("org.mockito:mockito-core:2.23.4")
+    testImplementation("com.github.stefanbirkner:system-rules:1.19.0")
+}
+
+application {
+    mainClassName = "cz.kramolis.exercises.paymenttracker.Main"
+
+}
+
+(tasks.getAt("run") as JavaExec).apply {
+    standardInput = System.`in`
+    systemProperty("paymentTracker.exchangeRate.USD.RMB", "0.14557")
+    systemProperty("paymentTracker.exchangeRate.USD.HKD", "0.12764")
+}
